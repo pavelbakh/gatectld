0a774691277d2df2dc073a94ba92f8783938be47
gugol.telecom@gmail.com <gugol.telecom@gmail.com@9fd6e136-b2d1-bb48-59e2-e0d44f1ea1b6>
2010-08-30 18:31:41 +0000

    version 0.0.1 working

--- src/adrport.h
+++ src/adrport.h
@@ -1,5 +1,10 @@
-// adrport.h
-// Copyright SPORTPLEX, SPORTPLEX by Sportplex Incorporated
+//============================================================================
+// Name        : adrport.h
+// Author      : Pavel Bakhmetiev
+// Version     : 1.0
+// Copyright   : Sportplex 2010 Copyright
+// Description : GATE control program in C, Ansi-style
+//============================================================================
 
 int OpenAdrPort (char* sPortNumber);
 int WriteAdrPort(char* psOutput);
